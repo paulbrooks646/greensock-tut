7adb4bada0340588f4e47c7bc152e1a190286ab9
chasedurrett <chase.durrett80@gmail.com>
2020-07-02 15:12:43 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Linear, TweenMax } from "gsap";
 
 function App() {
+  let logoElement = useRef(null);
+
+  useEffect(() => {
+    TweenMax.from(logoElement, 3, {
+      x: 600,
+      y: 600
+    })
+    TweenMax.to(logoElement, 6, {
+      repeat: -1,
+      rotate: 360,
+      ease: Linear.easeNone,
+    });
+  }, []);
+
+  function scaleUp() {
+    TweenMax.to(logoElement, 1, {
+      scale: 4,
+      ease: Linear.ease,
+    });
+  }
+  function scaleDown() {
+    TweenMax.to(logoElement, 1, {
+      scale: 0.2,
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img
+          src={logo}
+          className="App-logo"
+          alt="logo"
+          ref={(element) => {
+            logoElement = element;
+          }}
+          onMouseEnter={() => scaleUp()}
+          onMouseLeave={scaleDown}
+        />
       </header>
     </div>
   );
